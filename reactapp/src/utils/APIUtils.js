0eef529d3d71c378f89fdffadcd2e6b37bbbb1da
inkollusireeshaadharani <inkolludharani2508@gmail.com>
2023-07-14 16:12:40 +0000

    utils updated

---
+++ reactapp/src/utils/APIUtils.js
@@ -0,0 +1,37 @@
+const API_BASE_URL ="https://8080-bafcabaebbdbfcfdcdaadecbbaeeaadadfcaea.project.examly.io";
+const ACCESS_TOKEN ="";
+
+const request = (options) => {
+    const headers = new Headers({
+        'Content-Type': 'application/json',
+    })
+
+    const defaults = {headers: headers};
+    options = Object.assign({}, defaults, options);
+
+    return fetch(options.url, options)
+    .then(response => 
+        response.json().then(json => {
+            if(!response.ok) {
+                return Promise.reject(json);
+            }
+            return json;
+        })
+    );
+};
+
+export function login(loginRequest) {
+    return request({
+        url: API_BASE_URL + "/user/login",
+        method: 'POST',
+        body: JSON.stringify(loginRequest)
+    });
+}
+
+export function signup(signupRequest) {
+    return request({
+        url: API_BASE_URL + "/user/signup",
+        method: 'POST',
+        body: JSON.stringify(signupRequest)
+    });
+}
