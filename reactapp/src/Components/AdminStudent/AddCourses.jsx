79316f3a804dcbc2e578b60850173e3ec63024ad
yashwanth2637 <t.yashwanthreddy3@gmail.com>
2023-07-15 10:44:43 +0000

    Intial Commit

---
+++ reactapp/src/Components/AdminStudent/AddCourses.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Navbar from './Navbar'
+export default function AddCourses() {
+  return (
+    <>
+          <Navbar  Home="IAS Academy" Academy="Academy" Courses="Course" Students="Students" Logout="Logout"/>
+          courses.js
+
+    </>
+  )
+}
