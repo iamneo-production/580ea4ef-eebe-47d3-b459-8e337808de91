cbdfa2ac5d68731ca3fe25e06d7398dc4d5263a4
bhavanambindhureddy <bindhubhavanam2468@gmail.com>
2023-07-15 05:42:30 +0000

    Updated

---
+++ springapp/src/main/java/com/examly/springapp/repository/UserRepository.java
@@ -0,0 +1,17 @@
+package com.examly.springapp.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.examly.springapp.model.UserModel;
+
+@Repository
+public interface UserRepository extends JpaRepository<UserModel, Long> {
+
+    Optional<UserModel> findByEmail(String email);
+    
+    Optional<UserModel> findByEmailAndPassword(String email,String password);
+
+}
---
+++ springapp/src/main/java/com/examly/springapp/util/JwtUtil.java
@@ -0,0 +1,52 @@
+/* 
+package com.examly.springapp.util;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+@Component
+public class JwtUtil {
+
+    @Value("${jwt.secret}")
+    private String secret;
+
+    @Value("${jwt.expiration}")
+    private long expiration;
+
+    public String generateToken(String email, String userRole) {
+        Date now = new Date();
+        Date expiryDate = new Date(now.getTime() + expiration);
+
+        return Jwts.builder()
+                .setSubject(email)
+                .claim("role", userRole)
+                .setIssuedAt(now)
+                .setExpiration(expiryDate)
+                .signWith(SignatureAlgorithm.HS512, secret)
+                .compact();
+    }
+
+    public String getEmailFromToken(String token) {
+        Claims claims = Jwts.parser()
+                .setSigningKey(secret)
+                .parseClaimsJws(token)
+                .getBody();
+
+        return claims.getSubject();
+    }
+
+    public boolean validateToken(String token) {
+        try {
+            Jwts.parser().setSigningKey(secret).parseClaimsJws(token);
+            return true;
+        } catch (Exception ex) {
+            return false;
+        }
+    }
+} */
+
