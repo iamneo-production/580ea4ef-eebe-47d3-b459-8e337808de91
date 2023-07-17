54ef205049f4cd77e3f0c7e233e554e5e066ac2e
rajkumargadigi <rajkumargadigi96@gmail.com>
2023-07-17 17:06:48 +0000

    userAcademyUpdated

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -8,20 +8,29 @@ import AddAcademy from './components/AdminAcademy/AddAcademy';
 import EditAcademy from './components/AdminAcademy/EditAcademy';
 import AdminHome from './components/dummydashboard/AdminHome';
 import Home from './components/dummydashboard/Home';
+import AcademiesPage from "./components/UserAcademy/AcademiesPage"
 import AdminInstitute from './components/AdminAcademy/AdminInstitute';
+import MainPage from './components/UserAcademy/MainPage';
 function App() {
   return (
     <div className="App">
         <Router>
           <Routes>
             <Route element={<PrivateRoutes />}>
-                <Route element={<Home/>} path="/" exact/>
+                <Route element={<MainPage/>} path="/" exact/>
                  <Route element={<AdminHome/>} path="/adminhome"/>
             </Route>
             <Route element={<Login/>} path="/login"/>
             <Route element={<Signup/>} path="/signup"/>
             <Route element={<div>Page Not Found</div>} path="*"/>
+            <Route element={<AcademiesPage/>} path="/academies"/>
+            
+            
+
+
             <Route element={<Details/>} path="/Details"/>
+
+
             <Route element={<AdminInstitute/>} path="/AdminInstitute"/>
             <Route element={<AddAcademy/>} path ="/admin/addInstitute"/>
             <Route element={<EditAcademy/>} path="admin/editInstitute/:instituteId"/>
