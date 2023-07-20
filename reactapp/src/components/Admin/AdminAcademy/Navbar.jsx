3b449983947231c5dc72f211d8ecc21fe52e1e82
NisshanthJ <nisshanthjj@gmail.com>
2023-07-20 12:16:50 +0000

    Updated Frontend Functionalities

--- reactapp/src/components/Admin/AdminAcademy/Navbar.jsx
+++ reactapp/src/components/Admin/AdminAcademy/Navbar.jsx
@@ -7,16 +7,13 @@ import { Link } from 'react-router-dom';
 
 function Navigbar() {
   return (
-    <header>
-      <>
-        <Navbar collapseOnSelect expand="lg" style={{backgroundColor: "rgb(17, 178, 247)" }}>
-          <Container fluid>
-            <Navbar.Brand style={{ fontSize: "35px", paddingRight: "280px" }}>
-              IAS Academy
-            </Navbar.Brand>
-            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-            <Navbar.Collapse id="responsive-navbar-nav">
-              <Nav className="mr-auto" style={{ fontSize: "28px" }}>
+   
+      <div>
+        <Navbar expand="lg" style={{backgroundColor: "rgb(17, 178, 247)" }}>
+          
+            <Navbar.Brand>IAS Academy</Navbar.Brand>
+            <Navbar.Toggle aria-controls="navbarScroll" />
+        <Navbar.Collapse id="navbarScroll">
                 <Nav.Link id="adminAcademy" as={Link} to="/AdminInstitute" className="academy" style={{ color: "black", paddingRight: "30px" }}>
                   Academy
                 </Nav.Link>
@@ -26,17 +23,12 @@ function Navigbar() {
                 <Nav.Link id="adminStudents" as={Link} to="/Students" className="students" style={{ color: "black" }}>
                   Students
                 </Nav.Link>
-              </Nav>
-              <Nav>
-                <Nav.Link id="logout" as={Link} to="/logout" className="logout" style={{ fontWeight: "bold", color: "white"}}>
+                <Nav.Link id="logout" as={Link} to="/Login" className="logout" style={{ fontWeight: "bold", color: "black"}}>
                   Logout
                 </Nav.Link>
-              </Nav>
             </Navbar.Collapse>
-          </Container>
         </Navbar>
-      </>
-    </header>
+        </div>
   );
 }
 
