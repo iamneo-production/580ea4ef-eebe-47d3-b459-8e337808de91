df6f9f4d83b6191ec2900b44e72b42c3f7f293da
bhavanambindhureddy <bindhubhavanam2468@gmail.com>
2023-07-22 14:37:19 +0000

    Routes defined

--- reactapp/src/components/Admin/Admincourse/CourseForm.jsx
+++ reactapp/src/components/Admin/Admincourse/CourseForm.jsx
@@ -38,7 +38,7 @@ const AddCourseForm = () => {
         <Navbar.Brand>IAS Academy</Navbar.Brand>
         <Navbar.Toggle aria-controls="navbarScroll" />
         <Navbar.Collapse id="navbarScroll">
-          <Nav.Link id="adminAcademy" as={Link} to="/" className="academy" style={{ color: "black", paddingRight: "30px" }}>
+          <Nav.Link id="adminAcademy" as={Link} to="/AdminInstitute" className="academy" style={{ color: "black", paddingRight: "30px" }}>
                   Academy
                 </Nav.Link>
                 <Nav.Link id="adminCourse" as={Link} to="/Courses" className="courses" style={{ color: "black", paddingRight: "30px" }}>
@@ -47,7 +47,7 @@ const AddCourseForm = () => {
                 <Nav.Link id="adminStudents" as={Link} to="/Students" className="students" style={{ color: "black" }}>
                   Students
                 </Nav.Link>
-                <Nav.Link id="logout" as={Link} to="/logout" className="logout" style={{ fontWeight: "bold", color: "black"}}>
+                <Nav.Link id="logout" as={Link} to="/login" className="logout" style={{ fontWeight: "bold", color: "black"}}>
                   Logout
                 </Nav.Link>
         </Navbar.Collapse>
--- reactapp/src/components/Admin/Admincourse/EditCourse.jsx
+++ reactapp/src/components/Admin/Admincourse/EditCourse.jsx
@@ -53,7 +53,7 @@ const EditCourse = () => {
         <Navbar.Brand>IAS Academy</Navbar.Brand>
         <Navbar.Toggle aria-controls="navbarScroll" />
         <Navbar.Collapse id="navbarScroll">
-          <Nav.Link id="adminAcademy" as={Link} to="/" className="academy" style={{ color: "black", paddingRight: "30px" }}>
+          <Nav.Link id="adminAcademy" as={Link} to="/AdminInstitute" className="academy" style={{ color: "black", paddingRight: "30px" }}>
                   Academy
                 </Nav.Link>
                 <Nav.Link id="adminCourse" as={Link} to="/Courses" className="courses" style={{ color: "black", paddingRight: "30px" }}>
@@ -62,7 +62,7 @@ const EditCourse = () => {
                 <Nav.Link id="adminStudents" as={Link} to="/Students" className="students" style={{ color: "black" }}>
                   Students
                 </Nav.Link>
-                <Nav.Link id="logout" as={Link} to="/logout" className="logout" style={{ fontWeight: "bold", color: "black"}}>
+                <Nav.Link id="logout" as={Link} to="/login" className="logout" style={{ fontWeight: "bold", color: "black"}}>
                   Logout
                 </Nav.Link>
         </Navbar.Collapse>
