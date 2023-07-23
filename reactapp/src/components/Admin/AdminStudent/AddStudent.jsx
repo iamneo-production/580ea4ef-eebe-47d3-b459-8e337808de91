82c464166b29c23e1eec7e487e927746450eb664
yashwanth2637 <t.yashwanthreddy3@gmail.com>
2023-07-23 19:24:13 +0000

    Css updated

--- reactapp/src/components/Admin/AdminStudent/AddStudent.jsx
+++ reactapp/src/components/Admin/AdminStudent/AddStudent.jsx
@@ -3,8 +3,8 @@ import Navbar from './Navbar';
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
-import {API_BASE_URL} from "../../../utils/APIUtils";
 import './Studentdetails.css';
+
 
 const AddStudent = () => {
   let navigate = useNavigate();
@@ -29,7 +29,7 @@ const AddStudent = () => {
 
   const { firstName, lastName, gender, fatherName, phnNo1, phnNo2, motherName,
     email, age, houseNo, streetName, areaName, pinCode, state, nationality } = user;
-    
+
   const onInputChange = (e) => {
 
     setUser({ ...user, [e.target.name]: e.target.value });
@@ -37,220 +37,235 @@ const AddStudent = () => {
 
   const onSubmit = async (e) => {
     e.preventDefault();
-    await axios.post(`${API_BASE_URL}/user`, user)
-    .then((response) => {
-      console.log(response);
-      setUser(response.data);
-      navigate("/admin/viewStudent");
-    })
-    .catch((error) => {
-      console.log(error);
-    });
-   
+    await axios.post("http://localhost:8080/user", user)
+      .then((response) => {
+        console.log(response);
+        setUser(response.data);
+        navigate("/admin/viewStudent");
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+
   };
 
   return (
 
     <>
-      <div>
-        <Navbar
-          Home="IAS Academy"
-          Academy="Academy"
-          Courses="Courses"
-          Students="Students"
-          Logout="Logout"
-        />
-        <div className="details">
-          <form >
 
+      <Navbar
+        Home="IAS Academy"
+        Academy="Academy"
+        Courses="Course"
+        Students="Students"
+        Logout="Logout"
+      />
+      <div className="details">
+        <form>
+          <label>
             <input
               type={"text"}
               name="firstName"
               id='firstName'
               placeholder="Enter your First Name"
               autoComplete="off"
-              size="40"
+              size="35"
               value={firstName}
-              required 
+              required
               onChange={(e) => onInputChange(e)}
             />
-
+          </label>
+          <label>
             <input
               type={"text"}
               name="lastName"
               id='lastName'
               placeholder="Enter your Last Name"
-              size="40"
+              size="35"
               value={lastName}
-              required 
+              required
               onChange={(e) => onInputChange(e)}
             />
-
+          </label>
+          <label>
             <input
               type={"text"}
               name="gender"
               id='gender'
-              // placeholder="Enter male or female"
-              placeholder="Enter Gender"
-              size="40"
+              placeholder="Enter male or female"
+              size="35"
               value={gender}
-              required 
+              required
               onChange={(e) => onInputChange(e)}
             />
-
+          </label>
+          <label>
             <input
               type={"text"}
               name="fatherName"
               id='fatherName'
               placeholder="Enter your Father Name"
-              size="40"
-              required 
+              size="35"
+              required
               value={fatherName}
               onChange={(e) => onInputChange(e)}
             />
-
+          </label>
+          <label>
             <input
               type="tel"
               name="phnNo1"
               id='phoneNumber1'
               placeholder="Enter Phone Number"
-              size="40"
+              size="35"
               pattern="[0-9]{4}-[0-9]{3}-[0-9]{3}"
-              required 
+              required
               value={phnNo1}
               onChange={(e) => onInputChange(e)}
             />
-
+          </label>
+          <label>
             <input
               type="tel"
               name="phnNo2"
               id='phoneNumber2'
               placeholder="Enter Alternate Phone Number"
-              size="40"
+              size="35"
               value={phnNo2}
-              required 
+              required
               onChange={(e) => onInputChange(e)}
-            /><br />
-
+            />
+          </label>
+        </form>
+      </div>
+      <div className="container">
+        <form>
+          <label>
             <input type={"text"}
               name="motherName"
               id='motherName'
               placeholder="Enter your Mother Name"
-              size="40"
+              size="35"
               value={motherName}
-              required 
+              required
               onChange={(e) => onInputChange(e)}
-            /><br />
+            />
+          </label>
+
+          <label>
 
             <input
               type="email"
               name="email"
               id='emailId'
               placeholder="Enter your Email-ID"
-              autoComplete="off" size="40"
+              autoComplete="off"
+              size="35"
               value={email}
-              required 
+              required
               onChange={(e) => onInputChange(e)}
-            /><br />
-
+            />
+          </label>
+          <label>
             <input
               type="number"
               name="age"
               id='age'
-              placeholder= "Enter your Age"
-              size="40"
-              value={age}             
-              required 
+              placeholder="Enter your Age"
+              size="100"
+              width="25%"
+              value={age}
+              required
               onChange={(e) => onInputChange(e)}
             />
-          </form>
-        </div>
+          </label>
+        </form>
+      </div>
 
-        <div className="Address">
-          <form>          
-              <b>Address Information</b><br />
-            <label>
+      <div className="Address">
+        <form>
+          <b>Address Information</b>
+          <br/>
+          <label className='houseNo'>
             <b>House No :</b>
-            </label>
-            <input
-              type={"text"}
-              name="houseNo"
-              id='houseNo'
-              size="25"
-              value={houseNo}
-              required 
-              onChange={(e) => onInputChange(e)}
-            />
+          </label>
+          <input
+            type={"text"}
+            name="houseNo"
+            id='houseNo'
+            size="20"
+            value={houseNo}
+            required
+            onChange={(e) => onInputChange(e)}
+          />
 
-            <label>
-              <b>Street Name:</b>
-            </label>
-            <input
-              type={"text"}
-              name="streetName"
-              id='streetName' 
-              size="25"
-              value={streetName}
-              required 
-              onChange={(e) => onInputChange(e)}
-            /><br />
+          <label className='streetName'>
+            <b>Street Name:</b>
+          </label>
+          <input
+            type={"text"}
+            name="streetName"
+            id='streetName'
+            size="20"
+            value={streetName}
+            required
+            onChange={(e) => onInputChange(e)}
+          />
 
-            <label className='areaName'>
-              <b>Area Name:</b>
-            </label>
-            <input
-              type={"text"}
-              name="areaName"
-              id='areaName'
-              size="24"
-              value={areaName}
-              required 
-              onChange={(e) => onInputChange(e)}
-            />
+          <label className='areaName'>
+            <b>Area Name:</b>
+          </label>
+          <input
+            type={"text"}
+            name="areaName"
+            id='areaName'
+            size="20"
+            value={areaName}
+            required
+            onChange={(e) => onInputChange(e)}
+          />
 
-            <label className="pincode">
-              <b>Pincode:</b>
-            </label>
-            <input
-              type={"text"}
-              name="PinCode"
-              id='pinCode'
-              size="25"
-              value={pinCode}
-              
-              required 
-              onChange={(e) => onInputChange(e)}
-            /><br />
+          <label className="pincode">
+            <b>Pincode:</b>
+          </label>
+          <input
+            type={"text"}
+            name="PinCode"
+            id='pinCode'
+            size="20"
+            value={pinCode}
+            required
+            onChange={(e) => onInputChange(e)}
+          />
+          <br/>
+          <label className='state'>
+            <b>State:</b>
+          </label>
+          <input
+            type={"text"}
+            name="state"
+            id='state'
+            size="20"
+            value={state}
+            required
+            onChange={(e) => onInputChange(e)}
+          />
 
-            <label className='state'>
-              <b>State:</b>
-            </label>
-            <input
-              type={"text"}
-              name="state"
-              id='state'
-              size="25"
-              value={state}
-              required 
-              onChange={(e) => onInputChange(e)}
-            />
-
-            <label className='nationality'>
-              <b>Nationality:</b>
-            </label>
-            <input
-              type={"text"}
-              name="nationality"
-              id='nationality'
-              size="25"
-              value={nationality}
-              required 
-              onChange={(e) => onInputChange(e)}
-            />
-          </form>
-        </div >
+          <label className='nationality'>
+            <b>Nationality:</b>
+          </label>
+          <input
+            type={"text"}
+            name="nationality"
+            id='nationality'
+            size="20"
+            value={nationality}
+            required
+            onChange={(e) => onInputChange(e)}
+          />
+        </form>
       </div >
-      {/* //button */}
+
       <div className="addbtn2">
         <button type="submit" className="btn btn-light btn-lg" id="addstudentbtn" onClick={(e) => onSubmit(e)} >Add Student</button>
       </div>
