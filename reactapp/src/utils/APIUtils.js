62a9df349c6fab2aa2fec47c6db0e707b13a7154
Sanjay-33 <kumarsanjayp12@gmail.com>
2023-07-30 15:11:12 +0000

    Final commit

--- reactapp/src/utils/APIUtils.js
+++ reactapp/src/utils/APIUtils.js
@@ -1,4 +1,4 @@
-export const API_BASE_URL ="https://8080-eadabcbeccdddcdaadecbbaebabefadbfdcd.project.examly.io";
+export const API_BASE_URL ="https://8080-eadabcbeccdddcdaadecbbaeeaadadfcaea.project.examly.io";
 
 
 // export const API_BASE_URL ="https://8080-daefebfdcdcdaadecbbaeeaadadfcaea.project.examly.io";   
